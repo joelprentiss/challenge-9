4b4bc3665c3b63a90fccfc1c745f0e3c3c3fe40b
Joel Prentiss <joel.prentiss89@gmail.com>
2022-07-31 13:12:06 -0500

    2nd commit, updated script.js

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { type } = require('os');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your project title?',
+            name: 'projectTitle'
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running',
+            name: 'projectDescription'
+        },
+        {
+            type: 'input',
+            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            name: 'projectInstall',
+        },
+        {
+            type: 'input',
+            message: 'Provide instructions and examples for use. Include screenshots as needed.',
+            name: 'projectInst',
+        },
+        {
+            type: 'input',
+            message: 'List your collaborators, if any, with links to their GitHub profiles.',
+            name: 'projectCollab',
+        },
+
+    ])
+    .then((answers)=>{
+        console.log(answers);
+        fs.writeToFile('README.MD' ,JSON.stringify(writeMarkDown, null, '\t'), (err) =>
+  err ? console.error(err) : console.log('Success!'));
+    }) 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeMarkDown(fileName, data) {
+      const data = fs.writeToFile(answers);
+    const fileName = fs.writeFileSync(`./README/${answers.projectTitle}.md`);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
